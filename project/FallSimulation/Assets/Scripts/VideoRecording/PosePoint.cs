d9e72aa7f7388ec15708ab1ec358b048d69558ba
Flavia Brogle <22726959+flaviakim@users.noreply.github.com>
2026-03-25 13:53:52 +0100

    Change posepoint gizmo color

--- project/FallSimulation/Assets/Scripts/VideoRecording/PosePoint.cs
+++ project/FallSimulation/Assets/Scripts/VideoRecording/PosePoint.cs
@@ -5,8 +5,8 @@ namespace VideoRecording {
         [field:SerializeField] public int PoseID { get; private set; }
         
         private void OnDrawGizmos() {
-            Gizmos.color = Color.green;
-            Gizmos.DrawWireSphere(transform.position, 0.05f);
+            Gizmos.color = Color.blue;
+            Gizmos.DrawSphere(transform.position, 0.01f);
         }
 
         public Vector3 GetPosition() {
